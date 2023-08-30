854cd3115485429ad541c9bd4a3d9ba06056cbf9
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-08-30 18:02:15 -0300

    equilibrando o Saldo

--- EquilibrandoSaldo/src/saldo.java
+++ EquilibrandoSaldo/src/saldo.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class saldo {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        double saldoAtual = scanner.nextDouble();
+
+        double valorDeposito = scanner.nextDouble();
+
+        double valorRetirada = scanner.nextDouble();
+
+        scanner.close();
+
+        double saldo = ((saldoAtual + valorDeposito) - valorRetirada);
+
+        String resultado = String.format("%.1f", saldo);
+        System.out.println("Saldo autalizado na conta: " + resultado);
+
     }
 }
