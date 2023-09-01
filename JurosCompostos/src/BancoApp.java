c40b41e4403dc064437492b96eb6b371cf5fcb36
Gabriel Lima <gabrieldesouzalima@gmail.com>
2023-09-01 11:07:10 -0300

    correção

--- JurosCompostos/src/BancoApp.java
+++ JurosCompostos/src/BancoApp.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BancoApp {
-}
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BancoApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valorInicial = scanner.nextDouble();
+        double taxaJuros = Double.parseDouble(scanner.next());
+        double periodo = scanner.nextDouble();
+
+        double valorFinal = valorInicial * (Math.pow((1 + taxaJuros), periodo));
+
+        String resultado = String.format("%.2f", valorFinal);
+        System.out.println("Valor final do investimento: R$ " + resultado);
+
+        scanner.close();
+    }
+}git commitr
